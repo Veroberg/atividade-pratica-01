6eaf456df22efa3185e0d1b66bebf3aded733a5b
Verônica Bergelino <58296084+Veroberg@users.noreply.github.com>
2025-08-31 04:11:54 -0300

    Update 2_soma.py

--- 2_soma.py
+++ 2_soma.py
@@ -1,9 +1,25 @@
-# Calculadora de Soma
+# Atividade Prática 01 - Questão 2
+#
+# Desenvolva um programa que soma dois números.
+# Use as variáveis numero1 = 12 e numero2 = 14.
+# O programa deve calcular a soma e exibir o resultado.
+#
+# -----------------------------------------------------------------
 
+# Exemplo resolvido:
 numero1 = 12
 numero2 = 14
 soma = numero1 + numero2
 
+print("Exemplo resolvido:")
 print("Número 1:", numero1)
 print("Número 2:", numero2)
 print("Soma =", soma)
+print("-" * 20)
+
+# Código interativo:
+print("Opção para testar outros valores:")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print("Soma dos novos valores =", soma)
