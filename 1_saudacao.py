63c1c650cf0011706647730bfae7907533973f49
Verônica Bergelino <58296084+Veroberg@users.noreply.github.com>
2025-08-31 04:09:56 -0300

    Update 1_saudacao.py

--- 1_saudacao.py
+++ 1_saudacao.py
@@ -1,4 +1,8 @@
-# Programa de Saudação
-# Exemplo simples de saída em Python
+# Atividade Prática 01 - Questão 1
+#
+# Crie um programa que imprima a mensagem "Hello, world!" na tela.
+#
+# -----------------------------------------------------------------
 
+# Código criado para a atividade:
 print("Hello, world!")
